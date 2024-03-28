c43e87e94439c87777633d469478ab5971785207
iMaeGoo <mail1st@qq.com>
2024-03-28 14:56:45 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/haml.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/haml" instead of "highlight.js/lib/languages/haml.js"'))}emitWarning(),module.exports=require("./haml.js");
