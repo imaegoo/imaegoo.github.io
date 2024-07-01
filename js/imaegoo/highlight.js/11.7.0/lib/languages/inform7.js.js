86071cd2413133c9b6f4593b6e2f0db689a1b7ef
iMaeGoo <mail1st@qq.com>
2024-07-01 11:09:28 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/inform7.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/inform7" instead of "highlight.js/lib/languages/inform7.js"'))}emitWarning(),module.exports=require("./inform7.js");
