bfa47f68fc0a413b35b6acf386ebdb22bfbdfc39
iMaeGoo <mail1st@qq.com>
2024-08-09 15:16:02 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/stata.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/stata" instead of "highlight.js/lib/languages/stata.js"'))}emitWarning(),module.exports=require("./stata.js");
