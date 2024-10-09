4f29d919e331528cc180abd7976d89213b60f159
iMaeGoo <mail1st@qq.com>
2024-10-09 17:50:08 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/1c.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/1c" instead of "highlight.js/lib/languages/1c.js"'))}emitWarning(),module.exports=require("./1c.js");
---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/taggerscript.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/taggerscript" instead of "highlight.js/lib/languages/taggerscript.js"'))}emitWarning(),module.exports=require("./taggerscript.js");
