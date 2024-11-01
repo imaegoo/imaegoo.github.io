03220df52469d713579583b9c1cda0c73687e259
iMaeGoo <mail1st@qq.com>
2024-11-01 09:26:42 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/bnf.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/bnf" instead of "highlight.js/lib/languages/bnf.js"'))}emitWarning(),module.exports=require("./bnf.js");
