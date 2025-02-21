40eee1a587632884a0a0364b1af4998ee7b1c0bd
iMaeGoo <mail1st@qq.com>
2025-02-21 15:38:58 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/asciidoc.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/asciidoc" instead of "highlight.js/lib/languages/asciidoc.js"'))}emitWarning(),module.exports=require("./asciidoc.js");
