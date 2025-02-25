b06b52d3988b7de6b9943775b2c941f55bfbc3bd
iMaeGoo <mail1st@qq.com>
2025-02-25 10:36:19 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/n1ql.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/n1ql" instead of "highlight.js/lib/languages/n1ql.js"'))}emitWarning(),module.exports=require("./n1ql.js");
