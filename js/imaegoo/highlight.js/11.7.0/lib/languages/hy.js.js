7edf29ede6dc763e9f2987d5c1fa2271e48be131
iMaeGoo <mail1st@qq.com>
2025-05-09 16:28:24 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/hy.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/hy" instead of "highlight.js/lib/languages/hy.js"'))}emitWarning(),module.exports=require("./hy.js");
---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/shell.js
@@ -0,0 +1,1 @@
+function shell(s){return{name:"Shell Session",aliases:["console","shellsession"],contains:[{className:"meta.prompt",begin:/^\s{0,3}[/~\w\d[\]()@-]*[>%$#][ ]?/,starts:{end:/[^\\](?=\s*$)/,subLanguage:"bash"}}]}}module.exports=shell;
