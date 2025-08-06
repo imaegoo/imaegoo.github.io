fe49e75c79fdcf4e2086af66a4019e2cad53758c
iMaeGoo <mail1st@qq.com>
2025-08-06 11:03:52 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/graphql.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/graphql" instead of "highlight.js/lib/languages/graphql.js"'))}emitWarning(),module.exports=require("./graphql.js");
