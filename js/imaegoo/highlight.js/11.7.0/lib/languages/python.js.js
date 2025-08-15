4db0b4c45a92e7b0b2f38fafa3c8dbd19934b213
iMaeGoo <mail1st@qq.com>
2025-08-15 14:39:52 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/python.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/python" instead of "highlight.js/lib/languages/python.js"'))}emitWarning(),module.exports=require("./python.js");
