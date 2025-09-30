f1d21db2dd6facb4fb671b8cc295f4f39f0b1d8c
iMaeGoo <mail1st@qq.com>
2025-09-30 22:29:26 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/glsl.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/glsl" instead of "highlight.js/lib/languages/glsl.js"'))}emitWarning(),module.exports=require("./glsl.js");
