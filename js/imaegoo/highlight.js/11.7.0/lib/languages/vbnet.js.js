605c8c3baa65fb489f443254664322738f6d5242
iMaeGoo <mail1st@qq.com>
2026-02-04 10:04:44 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/vbnet.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/vbnet" instead of "highlight.js/lib/languages/vbnet.js"'))}emitWarning(),module.exports=require("./vbnet.js");
