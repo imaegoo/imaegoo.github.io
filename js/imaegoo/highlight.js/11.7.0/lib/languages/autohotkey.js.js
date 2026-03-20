35eb32526b4e9b1d76e12307ed9e8e587891b32a
iMaeGoo <mail1st@qq.com>
2026-03-20 16:07:18 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/autohotkey.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/autohotkey" instead of "highlight.js/lib/languages/autohotkey.js"'))}emitWarning(),module.exports=require("./autohotkey.js");
