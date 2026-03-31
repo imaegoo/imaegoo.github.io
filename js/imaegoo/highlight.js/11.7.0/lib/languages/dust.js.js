d9fca5cb7b1a824c89448d61083e25c861cafdaa
iMaeGoo <mail1st@qq.com>
2026-03-31 15:23:15 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/dust.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/dust" instead of "highlight.js/lib/languages/dust.js"'))}emitWarning(),module.exports=require("./dust.js");
