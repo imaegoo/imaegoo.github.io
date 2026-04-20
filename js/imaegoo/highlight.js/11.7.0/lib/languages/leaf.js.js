145f3475aa35a596af2b8c8e8da499a3f4d4cca8
iMaeGoo <mail1st@qq.com>
2026-04-20 21:08:19 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/leaf.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/leaf" instead of "highlight.js/lib/languages/leaf.js"'))}emitWarning(),module.exports=require("./leaf.js");
