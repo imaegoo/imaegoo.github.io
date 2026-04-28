14eec1ec305eeceb7b0f4b62d88373d7d2c29cf3
iMaeGoo <mail1st@qq.com>
2026-04-28 15:30:21 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/groovy.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/groovy" instead of "highlight.js/lib/languages/groovy.js"'))}emitWarning(),module.exports=require("./groovy.js");
---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/wasm.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/wasm" instead of "highlight.js/lib/languages/wasm.js"'))}emitWarning(),module.exports=require("./wasm.js");
