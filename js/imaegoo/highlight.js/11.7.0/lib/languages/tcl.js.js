9990fd9986826a98880e15d1b5573a929820a92b
iMaeGoo <mail1st@qq.com>
2026-05-14 14:43:31 +0800

    imaegoo

---
+++ js/imaegoo/highlight.js/11.7.0/lib/languages/tcl.js.js
@@ -0,0 +1,1 @@
+function emitWarning(){emitWarning.warned||(emitWarning.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/tcl" instead of "highlight.js/lib/languages/tcl.js"'))}emitWarning(),module.exports=require("./tcl.js");
